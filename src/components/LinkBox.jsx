da949b4e3c6e968e4cb31c0c62c09d4b7046860b
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-15 05:44:29 +0100

    Add dark theme stylings for all elements and dark theme switching in LinkBox.

--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from './../ThemeContext';
 
 const LinkBox = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="links-light">
+    <div className={`links-${theme}`}>
     <ul className="links-list">
       <li className="links-list-item">
         <Link to="/me">ME</Link>
