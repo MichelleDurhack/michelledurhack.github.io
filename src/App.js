9e12fca2167c5cddca1dcbb244b28a9141d3b014
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-15 05:29:46 +0100

    Add context for theme.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Route, Switch } from "react-router-dom";
+import { ThemeContext } from "./ThemeContext";
 import WebDeveloper from './components/WebDeveloper';
 import GraphicDesign from './components/GraphicDesign';
 import Handletterer from './components/Handletterer';
@@ -6,19 +8,23 @@ import AboutMe from './components/AboutMe';
 import LinkBox from './components/LinkBox';
 
 function App() {
+  const [ theme, setTheme ] = useState("light");
+
   return (
-    <div className="content">
-      <p>Michelle Durhack</p>
-      <div className="main">
-        <Switch>
-          <Route path="/webdev" component={WebDeveloper}/>
-          <Route path="/design" component={GraphicDesign}/>
-          <Route path="/lettering" component={Handletterer}/>
-          <Route path="/me" component={AboutMe}/>
-          <Route path="/" exact component={LinkBox}/>
-        </Switch>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`content-${theme}`}>
+        <p>Michelle Durhack</p>
+        <div className="main">
+          <Switch>
+            <Route path="/webdev" component={WebDeveloper}/>
+            <Route path="/design" component={GraphicDesign}/>
+            <Route path="/lettering" component={Handletterer}/>
+            <Route path="/me" component={AboutMe}/>
+            <Route path="/" exact component={LinkBox}/>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
