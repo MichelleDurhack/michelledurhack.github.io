3bf7a0394c199aad4167ad99f0a5207add813bb2
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-15 05:25:19 +0100

    Implement LinkBox component with styling and general styling changes.

--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom";
+
 const LinkBox = () => {
   return (
-    <div>Links</div>
+    <div className="links-light">
+    <ul className="links-list">
+      <li className="links-list-item">
+        <Link to="/me">ME</Link>
+      </li>
+      <li className="links-list-item">
+        <Link to="/lettering">HANDLETTERER</Link>
+      </li>
+      <li className="links-list-item">
+        <Link to="/design"> GRAPHIC DESIGNER</Link>
+      </li>
+      <li className="links-list-item">
+        <Link to="/webdev">FULLSTACK WEBDEVELOPER</Link>
+      </li>
+    </ul>
+  </div>
   );
 }
  
