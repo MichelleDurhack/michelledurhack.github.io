6d086d4abb316e4565d9fcc70b9e595262c1d4fc
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-16 02:48:07 +0100

    Add cards and hover functionality.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,76 @@
 const AboutMe = () => {
   return (
-    <h1>About Me</h1>
+    <div className="container">
+
+      <div className="card">
+        <div className="face face1">
+          <div className="content">
+            <span className="stars"></span>
+            <h2 className="handlettering">Handlettering</h2>
+            <ul className="handlettering">
+              <li>Brushlettering</li>
+              <li>Calligraphy</li>
+              <li>Faux Calligraphy</li>
+              <li>Creative Lettering</li>
+            </ul>
+          </div>
+        </div>
+        <div className="face face2">
+          <h2>01</h2>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="face face1">
+          <div className="content">
+            <span className="stars"></span>
+            <h2 className="webdev">Web Development</h2>
+            <ul className="webdev">
+              <li>MongoDB</li>
+              <li>ExpressJS</li>
+              <li>ReactJS</li>
+              <li>NodeJS</li>
+              <li>NPM</li>
+              <li>C</li>
+              <li>Python</li>
+              <li>PHP</li>
+              <li>MySQL</li>
+              <li>Flask</li>
+              <li>Javascript (ES6)</li>
+              <li>RestAPI</li>
+              <li>Git</li>
+              <li>Visual Studio Code</li>
+              <li>HTML</li>
+              <li>CSS/SASS</li>
+            </ul>
+          </div>
+        </div>
+        <div className="face face2">
+          <h2>02</h2>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="face face1">
+          <div className="content">
+            <span className="stars"></span>
+            <h2 className="design">Design</h2>
+            <ul className="design">
+              <li>Adobe Illustrator</li>
+              <li>Adobe Photoshop</li>
+              <li>Adobe InDesign</li>
+              <li>Adobe XD</li>
+              <li>Adobe Dimension</li>
+            </ul>
+          </div>
+        </div>
+        <div className="face face2">
+          <h2>03</h2>
+        </div>
+      </div>
+
+    </div>/*Container Closing Tag*/
   );
 }
  
-export default AboutMe;
+export default AboutMe; 
