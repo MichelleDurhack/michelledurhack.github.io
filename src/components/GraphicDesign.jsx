dbd9fc071c035ecccfdd5dc1e97cc4a611dd3e9e
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-18 12:48:17 +0100

    Scrap design on GraphicDesign component.

--- src/components/GraphicDesign.jsx
+++ src/components/GraphicDesign.jsx
@@ -3,48 +3,8 @@ import { Fragment } from "react";
 const GraphicDesign = () => {
   return (
     <Fragment>
-      <div className="headerbox">
-        <h2>Graphic Design</h2>
-      </div>
-      <div className="main">
+      
 
-      <div className="graphCardContainer">
-        <div id="vapor1" className="graphicCard">
-          <div className="conFront front">
-            <div className="mainContent">
-            </div>
-          </div>
-          <div className="conFront back">
-            <h2>Vaporwave</h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="graphCardContainer">
-        <div id="vapor2" className="graphicCard">
-          <div className="conFront front">
-            <div className="mainContent">
-            </div>
-          </div>
-          <div className="conFront back">
-            <h2>Vaporwave</h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="graphCardContainer">
-        <div id="vapor3" className="graphicCard">
-          <div className="conFront front">
-            <div className="mainContent">
-            </div>
-          </div>
-          <div className="conFront back">
-            <h2>Vaporwave</h2>
-          </div>
-        </div>
-      </div>
-
-      </div>
     </Fragment>
   );
 }
