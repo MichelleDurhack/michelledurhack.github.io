703196e77b38535db54d4a27ea25adb3632f9963
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-18 06:04:41 +0100

    Create first row of graphic design work examples.

--- src/components/GraphicDesign.jsx
+++ src/components/GraphicDesign.jsx
@@ -9,16 +9,39 @@ const GraphicDesign = () => {
       <div className="main">
 
       <div className="graphCardContainer">
-
-        <div className="graphicCard">
+        <div id="vapor1" className="graphicCard">
           <div className="conFront front">
-            <div className="mainContent"></div>
+            <div className="mainContent">
+            </div>
           </div>
           <div className="conFront back">
             <h2>Vaporwave</h2>
           </div>
         </div>
+      </div>
 
+      <div className="graphCardContainer">
+        <div id="vapor2" className="graphicCard">
+          <div className="conFront front">
+            <div className="mainContent">
+            </div>
+          </div>
+          <div className="conFront back">
+            <h2>Vaporwave</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="graphCardContainer">
+        <div id="vapor3" className="graphicCard">
+          <div className="conFront front">
+            <div className="mainContent">
+            </div>
+          </div>
+          <div className="conFront back">
+            <h2>Vaporwave</h2>
+          </div>
+        </div>
       </div>
 
       </div>
