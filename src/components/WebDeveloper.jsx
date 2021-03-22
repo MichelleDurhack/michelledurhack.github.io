badaa90cf5bb6afcf4aab91a0afac8599d40b5ef
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-22 16:29:27 +0100

    Add darkmode to WebDev component.

--- src/components/WebDeveloper.jsx
+++ src/components/WebDeveloper.jsx
@@ -1,15 +1,17 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import { ThemeContext } from './../ThemeContext';
 
 const WebDeveloper = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <Fragment>
       <div className="headerbox">
-        <h1 id="devCaption">Fullstack Web Developer</h1>
+        <h1 id={`devCaption-${theme}`}>Fullstack Web Developer</h1>
       </div>
 
       <div className="main">
         <div className="textBlock">
-          <p>There isn't a lot to show yet. Have you seen my website 
+          <p className={`textarea-${theme}`}>There isn't a lot to show yet. Have you seen my website 
             tho? Pretty cool huh? Made with React, SASS/SCSS and cool 
             gradients. Check back here later, more projects to come.
             Also check out my skills page, where you can see what my 
