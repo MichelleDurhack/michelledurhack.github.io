5422220a01f009f98e081c505ab5543503788824
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-22 11:13:02 +0100

    Create first image-text-combo in GD component.

--- src/components/GraphicDesign.jsx
+++ src/components/GraphicDesign.jsx
@@ -3,7 +3,25 @@ import { Fragment } from "react";
 const GraphicDesign = () => {
   return (
     <Fragment>
-      
+      <div className="main">
+        <div className="imageGrid">
+
+          <div id="vapor1" className="imageCard">
+            <img alt="something" className="image frontfacing" src="/vapor1.jpg" />         
+            <div className="image backfacing"></div>
+          </div>
+          <div className="imageText">
+            <div className="textBox">
+              <span className="headerText image1">Free Work</span>
+              <span className="subheaderText image1">Genre-Vaporwave</span>
+              <p className="image1">First attempt at creating a vaporwave collage.</p>
+            </div>
+          </div>
+
+          
+
+        </div>
+      </div>      
 
     </Fragment>
   );
