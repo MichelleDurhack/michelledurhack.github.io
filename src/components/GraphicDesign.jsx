b83aa3a2e00f4897993823bb24c689d0d2d91341
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-22 11:22:57 +0100

    Add 2nd image-text-combo and fix styling bug.

--- src/components/GraphicDesign.jsx
+++ src/components/GraphicDesign.jsx
@@ -10,15 +10,25 @@ const GraphicDesign = () => {
             <img alt="something" className="image frontfacing" src="/vapor1.jpg" />         
             <div className="image backfacing"></div>
           </div>
-          <div className="imageText">
+          <div className="imageText image1">
             <div className="textBox">
-              <span className="headerText image1">Free Work</span>
-              <span className="subheaderText image1">Genre-Vaporwave</span>
-              <p className="image1">First attempt at creating a vaporwave collage.</p>
+              <span className="headerText">Free Work</span>
+              <span className="subheaderText">Genre-Vaporwave</span>
+              <p>First attempt at creating a vaporwave collage.</p>
             </div>
           </div>
 
-          
+          <div id="vapor2" className="imageCard">
+            <img alt="something" className="image frontfacing" src="/vapor2.jpg" />         
+            <div className="image backfacing"></div>
+          </div>
+          <div className="imageText image2">
+            <div className="textBox">
+              <span className="headerText">Free Work</span>
+              <span className="subheaderText">Genre-Vaporwave</span>
+              <p>First attempt at creating a vaporwave collage.</p>
+            </div>
+          </div>
 
         </div>
       </div>      
