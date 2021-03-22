59531c098691195ddd3674704399aa8d0f7a3f6b
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-22 16:30:38 +0100

    Fix typo in paragraph.

--- src/components/WebDeveloper.jsx
+++ src/components/WebDeveloper.jsx
@@ -14,7 +14,7 @@ const WebDeveloper = () => {
           <p className={`textarea-${theme}`}>There isn't a lot to show yet. Have you seen my website 
             tho? Pretty cool huh? Made with React, SASS/SCSS and cool 
             gradients. Check back here later, more projects to come.
-            Also check out my skills page, where you can see what my 
+            Make sure to check out my skills page, where you can see what my 
             other skills are!</p>
           <img src="https://media.giphy.com/media/111ebonMs90YLu/giphy.gif" alt="Thumbs up gif"/>
         </div>
