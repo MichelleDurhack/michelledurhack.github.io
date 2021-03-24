62358af0d86c2af627279d16286f8c513ccabe6b
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-24 19:48:42 +0100

    Add darkmode in component.

--- src/components/Handletterer.jsx
+++ src/components/Handletterer.jsx
@@ -1,10 +1,13 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import { ThemeContext } from "./../ThemeContext";
 
 const Handletterer = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <Fragment>
       <div className="headerbox">
-        <h2 className="handletterCaption">My handlettering works</h2>
+        <h2 id={`handletterCaption-${theme}`}>My handlettering works</h2>
       </div>
 
       <div className="main">
@@ -12,11 +15,11 @@ const Handletterer = () => {
 
           <div id="moodtracker1" className="letteringCard">
             <img className="lettering frontwards" src="media/moodtracker1.jpg" alt="Moodtracker Header"/>
-            <div id="backwards1" className="lettering backwards"></div>
+            <div id="backwards1" className={`lettering journal-${theme} backwards`}></div>
           </div>
           <div id="letteringText1" className="letteringText">
-            <div className="letteringBox">
-              <span className="letHeader">Free Work</span>
+            <div className={`letteringBox journalText-${theme}`}>
+              <span className={`letHeader journalHeader-${theme}`}>Free Work</span>
               <span className="letSubheader">bulletjournal</span>
               <p className="letText">When starting my bulletjournal I experimented with a lot of different header styles.</p>
             </div>
@@ -24,11 +27,11 @@ const Handletterer = () => {
 
           <div id="moodtracker2" className="letteringCard">
             <img className="lettering frontwards" src="media/moodtracker2.jpg" alt="Moodtracker Header"/>
-            <div id="backwards2" className="lettering backwards"></div>
+            <div id="backwards2" className={`lettering journal-${theme} backwards`}></div>
           </div>
           <div id="letteringText2" className="letteringText">
-            <div className="letteringBox">
-              <span className="letHeader">Free Work</span>
+            <div className={`letteringBox journalText-${theme}`}>
+              <span className={`letHeader journalHeader-${theme}`}>Free Work</span>
               <span className="letSubheader">bulletjournal</span>
               <p className="letText">Another one of my header experimentations.</p>
             </div>
@@ -36,11 +39,11 @@ const Handletterer = () => {
 
           <div id="quote1" className="letteringCard">
             <img className="lettering frontwards" src="media/quote1.jpg" alt="Moodtracker Header"/>
-            <div id="backwards3" className="lettering backwards"></div>
+            <div id="backwards3" className={`lettering journal-${theme} backwards`}></div>
           </div>
           <div id="letteringText3" className="letteringText">
-            <div className="letteringBox">
-              <span className="letHeader">Free Work</span>
+            <div className={`letteringBox journalText-${theme}`}>
+              <span className={`letHeader journalHeader-${theme}`}>Free Work</span>
               <span className="letSubheader">bulletjournal</span>
               <p className="letText">One of the pages in my calendar setup. By far my favourite typeface creation.</p>
             </div>
@@ -48,15 +51,15 @@ const Handletterer = () => {
 
           <div id="worksample1" className="letteringCard">
             <img className="lettering etikette frontwards" src="media/ErdbeerEtikett.png" alt="Moodtracker Header"/>
-            <div className="lettering etiketteback backwards"></div>
+            <div className={`lettering etikette-${theme} etiketteback backwards`}></div>
           </div>
           <div id="worksample2" className="letteringCard">
             <img className="lettering etikette frontwards" src="media/KirschEtikett.png" alt="Moodtracker Header"/>
-            <div className="lettering etiketteback backwards"></div>
+            <div className={`lettering etikette-${theme} etiketteback backwards`}></div>
           </div>
           <div id="letteringText4" className="letteringText">
-            <div className="letteringBox">
-              <span className="letHeader">Free Work</span>
+            <div className={`letteringBox etiketteText-${theme}`}>
+              <span className={`letHeader etiketteHeader-${theme}`}>Free Work</span>
               <span className="letSubheader">etiquette design</span>
               <p className="letText">Creating a jam etiquette for a fake, all natural farming company. The design was inspired by mordern start up companies that have a strong handmade feel.</p>
             </div>
