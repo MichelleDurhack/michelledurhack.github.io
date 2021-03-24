3a596378c94ff8791209a8487aad3e726901143c
Michelle Durhack <michelle.durhack@gmail.com>
2021-03-24 19:53:52 +0100

    Add darkmode to home button.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={`background bkg-${theme}`}>
         <div className={`content content-${theme}`}>
-          <NavLink to="/" className="mainpageLink">Michelle Durhack</NavLink>
+          <NavLink to="/" className={`mainpageLink homeLink-${theme}`}>Michelle Durhack</NavLink>
           <div className="componentBox">
             <Switch>
               <Route path="/webdev" component={WebDeveloper}/>
